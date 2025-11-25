836c680a30714871df22b959229dfb56a2fdbcd9
cmj <>
2025-11-25 01:46:51 -0800

    More features for settings output

--- user/getSettings.sh
+++ user/getSettings.sh
@@ -6,10 +6,10 @@ auth_token=$1
 ###
 ct0=$(tr -dc 0-9a-f < /dev/urandom | head -c 32)
 bearer_token=AAAAAAAAAAAAAAAAAAAAAFQODgEAAAAAVHTp76lzh3rFzcHbmHVvQxYYpTw%3DckAlMINMjmCwxUcaXbAN4XqJVdgMJaHqNOFgPMK0zN1qLqLQCF
-#bearer_token=AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA
 
-curl https://api.x.com/1.1/account/settings.json \
+curl -s 'https://api.x.com/1.1/account/settings.json?include_ext_sharing_audiospaces_listening_data_with_followers=true&include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&include_ext_dm_av_call_settings=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true' \
   -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
   -H "x-csrf-token: ${ct0}" \
   -H "authorization: Bearer ${bearer_token}" \
-  -H "Cookie: ct0=${ct0}; auth_token=${auth_token}"
+  -H "Cookie: ct0=${ct0}; auth_token=${auth_token}" |
+  jq
