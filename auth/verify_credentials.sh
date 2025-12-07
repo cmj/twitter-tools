dcfdcc187b6fec12fac14b3284657700858fafea
cmj <>
2025-12-07 05:09:32 -0800

    Fix verify_credentials/isLoggedIn example

--- auth/verify_credentials.sh
+++ auth/verify_credentials.sh
@@ -4,15 +4,13 @@
 auth_token=$1
 
 #x_csrf_token=''
-# or basic csrf token. this 32-byte string can be random, unlike the
-# 160-byte x-csrf-token you are given.
+# or basic csrf token. this 32-character string can be random
 x_csrf_token='00000000000000000000000000000000' 
 
-bearer_token="AAAAAAAAAAAAAAAAAAAAAFQODgEAAAAAVHTp76lzh3rFzcHbmHVvQxYYpTw%3DckAlMINMjmCwxUcaXbAN4XqJVdgMJaHqNOFgPMK0zN1qLqLQCF"
-
+bearer_token="AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F"
 curl -s \
   -H "Authorization: Bearer ${bearer_token}" \
-  -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
+  -H "User-Agent: TwitterAndroid/10.21.1" \
   -H "X-Csrf-Token: ${x_csrf_token}" \
   -H "Cookie: ct0=${x_csrf_token}; auth_token=${auth_token}" \
   'https://api.twitter.com/1.1/account/verify_credentials.json' |
