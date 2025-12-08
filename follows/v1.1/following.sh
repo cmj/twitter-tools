85468fcb7f4cc208770d84e03c9746e21e47c284
cmj <129799+cmj@users.noreply.github.com>
2025-12-08 04:23:47 -0800

    v1.1 following request header fix

--- follows/v1.1/following.sh
+++ follows/v1.1/following.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # v1.1 now requires curl major_version >=8 or curl-impersonate
 
-x_csrf_token=''
-auth_token=''
+#x_csrf_token=''
+#auth_token=''
+
+. ~/.env-twitter
+
+###
 
 usage() { echo "$0 twitter_username"; exit 1; }
 [ "$#" -ne 1 ] && usage
 user="$1"
 
 bearer_token='AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F'
-header=(-H "Authorization: Bearer ${bearer_token}" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" -H "X-Csrf-Token: ${x_csrf_token}" -H "Cookie: ct0=${x_csrf_token}; auth_token=${auth_token}")
+header=(-H "Authorization: Bearer ${bearer_token}" -H "User-Agent: TwitterAndroid/10.21.1" -H "X-Csrf-Token: ${x_csrf_token}" -H "Cookie: ct0=${x_csrf_token}; auth_token=${auth_token}")
 
 lookup=$(curl -s "https://api.twitter.com/1.1/users/lookup.json?screen_name=${user//@/}" "${header[@]}")
 user_id=$(jq '.[].id' <<< "${lookup}")
