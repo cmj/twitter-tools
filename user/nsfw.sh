b54b8b969fbfe5ded5bcb9f9d2d0daeb0422ecab
cmj <129799+cmj@users.noreply.github.com>
2025-12-27 00:16:08 -0800

    fix for getSettings and nsfw toggle

--- user/nsfw.sh
+++ user/nsfw.sh
@@ -4,14 +4,16 @@
 
 auth_token=$1
 
+if [[ -z $1 ]]; then echo -e "Enable sensitive content\n$0 <auth_token>"; exit 1; fi
+
 ###
 ct0=$(tr -dc 0-9a-f < /dev/urandom | head -c 32)
-#bearer_token="AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
-bearer_token="AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F"
+bearer_token="AAAAAAAAAAAAAAAAAAAAAIWCCAAAAAAA2C25AxqI%2BYCS7pdfJKRH8Xh19zA%3D8vpDZzPHaEJhd20MKVWp3UR38YoPpuTX7UD2cVYo3YNikubuxd"
 
 curl -s https://api.x.com/1.1/account/settings.json \
   -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
   -H "x-csrf-token: ${ct0}" \
   -H "authorization: Bearer ${bearer_token}" \
   -H "Cookie: ct0=${ct0}; auth_token=${auth_token}" \
-  -d 'include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&display_sensitive_media=true'
+  -d 'include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&display_sensitive_media=true' |
+  jq
