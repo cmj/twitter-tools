8d40b3a7c730c39099de363d500aa1c57a2eea5d
cmj <129799+cmj@users.noreply.github.com>
2026-01-02 08:09:56 -0800

    Only use x-client-transaction-id with web token

--- misc/endpoints.sh
+++ misc/endpoints.sh
@@ -7,24 +7,24 @@ x_csrf_token=$(openssl rand -hex 16)
 query="fish"
 product="Latest" # Latest | Top
 count=20 # 1-20
-#user_id=783214 # X
+user_id2=783214 # X
 user_id=12 # @jack
-tweet_id=20 # @jack - just setting up my twttr
+tweet_id=20 # @jack (also used for conversation_id)
 screen_name="jack"
 list_id=1860883 # @mashable - Social Media
 list_slug="social-media"
 
-# Main - all work with x-client-transaction-id
-bearer_token='AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
-
 # Twitter for Android - all work
-#bearer_token='AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F'
+bearer_token='AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F'
 
 # Twitter for iPhone - all work
 #bearer_token="AAAAAAAAAAAAAAAAAAAAAAj4AQAAAAAAPraK64zCZ9CSzdLesbE7LB%2Bw4uE%3DVJQREvQNCZJNiz3rHO7lOXlkVOQkzzdsgu6wWgcazdMUaGoUGm"
 
 # Twitter for Mac - 3 only - UserWithProfileTweetsAndRepliesQueryV2 ListByRestId Retweeters
 #bearer_token="AAAAAAAAAAAAAAAAAAAAAIWCCAAAAAAA2C25AxqI%2BYCS7pdfJKRH8Xh19zA%3D8vpDZzPHaEJhd20MKVWp3UR38YoPpuTX7UD2cVYo3YNikubuxd"
+
+# Web main.js - TweetResultByRestId UserTweets (all work with TID enabled, except ConversationTimelineV2)
+#bearer_token='AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
 
 ####
 if [[ -z "$auth_token" ]]; then echo "Usage: $0 auth_token"; exit 1; fi
@@ -34,10 +34,10 @@ path='/i/api/graphql/'
 url="${domain}${path}"
 endpoints=(
   '-oaLodhGbbnzJBACb1kk2Q/UserByScreenName'
-  'xavgLWWbFH8wm_8MQN8plQ/UsersByRestIds'
-  'ujL_oXbgVlDHQzWSTgzvnA/UsersByScreenNames'
   'u7wQyGi6oExe8_TRWGMq4Q/UserResultByScreenNameQuery'
   'oPppcargziU1uDQHAUmH-A/UserResultByIdQuery'
+  'xavgLWWbFH8wm_8MQN8plQ/UsersByRestIds'
+  'ujL_oXbgVlDHQzWSTgzvnA/UsersByScreenNames'
   '3JNH4e9dq1BifLxAa3UMWg/UserWithProfileTweetsQueryV2'
   '8IS8MaO-2EN6GZZZb8jF0g/UserWithProfileTweetsAndRepliesQueryV2'
   '36oKqyQ7E_9CmtONGjJRsA/UserMedia'
@@ -53,28 +53,35 @@ endpoints=(
   'oRJs8SLCRNRbQzuZG93_oA/UserTweets'
   'kkaJ0Mf34PZVarrxzLihjg/UserTweetsAndReplies'
   'Y4Erk_-0hObvLpz0Iw3bzA/ConversationTimeline'
+  'k3rtLsS9kG5hI-Jr0dTMCg/ConversationTimelineV2'
   'wfglZEC0MRgBdxMa_1a5YQ/Retweeters'
   'WJbdU-1ay4MHL8nKqCZYUQ/Following'
   'kuFUYP9eV1FPoEy4N-pi7w/Followers'
+  '_-RuCOiTBRLm2IarGaeB3w/SimilarPosts'
   'qZ92r6KDO0_GZxVJGM33XA/AboutAccountQuery'
 )
 
 request_endpoint() {
   # uncomment to save output
-  #json_file="/tmp/${opt##*/}-$EPOCHSECONDS.json"
-
+  #json_file="/tmp/${endpoint##*/}-$EPOCHSECONDS.json"
+  #json_file="${endpoint##*/}-$EPOCHSECONDS.json"
+  
   URL="${url}${endpoint}"
-  TID=$(curl -s "https://x-client-transaction-id-generator.xyz/generate-x-client-transaction-id?path=${path}${endpoint}" | jq -r '."x-client-transaction-id"')
-  VARIABLES='{"screen_name":"'"${screen_name}"'","screenName":"'"${screen_name}"'","screen_names":["x","jack"],"rawQuery":"'"${query}"'","query":"'"${query}"'","userId":"'"${user_id}"'","userIds":["'"${user_id}"'","'"${user_id2}"'"],"rest_id":"'"${user_id}"'","postId":"'"${tweet_id}"'","focalTweetId":"'"${tweet_id}"'","tweetId":"'"${tweet_id}"'","query_source":"typed_query","count":'"${count}"',"querySource":"typed_query","product":"'"${product}"'","listId":"'"${list_id}"'","includePromotedContent":false,"withBirdwatchNotes":true,"withVoice":true,"withCommunity":false,"listSlug":"'"${list_slug}"'","withDownvotePerspective":true,"withReactionsMetadata":true,"withReactionsPerspective":true}'
-  FEATURES='{"android_graphql_skip_api_media_color_palette":false,"blue_business_profile_image_shape_enabled":false,"creator_subscriptions_subscription_count_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"freedom_of_speech_not_reach_fetch_enabled":false,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":false,"hidden_profile_likes_enabled":false,"highlights_tweets_tab_ui_enabled":false,"interactive_text_enabled":false,"longform_notetweets_consumption_enabled":true,"longform_notetweets_inline_media_enabled":false,"longform_notetweets_richtext_consumption_enabled":true,"longform_notetweets_rich_text_read_enabled":false,"responsive_web_edit_tweet_api_enabled":false,"responsive_web_enhance_cards_enabled":false,"responsive_web_graphql_exclude_directive_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"responsive_web_graphql_timeline_navigation_enabled":false,"responsive_web_media_download_video_enabled":false,"responsive_web_text_conversations_enabled":false,"responsive_web_twitter_article_tweet_consumption_enabled":false,"responsive_web_twitter_blue_verified_badge_is_enabled":true,"rweb_lists_timeline_redesign_enabled":true,"spaces_2022_h2_clipping":true,"spaces_2022_h2_spaces_communities":true,"standardized_nudges_misinfo":false,"subscriptions_verification_info_enabled":true,"subscriptions_verification_info_reason_enabled":true,"subscriptions_verification_info_verified_since_enabled":true,"super_follow_badge_privacy_enabled":false,"super_follow_exclusive_tweet_notifications_enabled":false,"super_follow_tweet_api_enabled":false,"super_follow_user_api_enabled":false,"tweet_awards_web_tipping_enabled":false,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":false,"tweetypie_unmention_optimization_enabled":false,"unified_cards_ad_metadata_container_dynamic_card_content_query_enabled":false,"verified_phone_label_enabled":false,"vibe_api_enabled":false,"view_counts_everywhere_api_enabled":false,"c9s_tweet_anatomy_moderator_badge_enabled":false,"rweb_video_timestamps_enabled":false,"rweb_tipjar_consumption_enabled":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"articles_preview_enabled":false,"tweet_with_visibility_results_prefer_gql_media_interstitial_enabled":false,"communities_web_enable_tweet_community_results_fetch":false,"responsive_web_grok_analyze_post_followups_enabled":false,"responsive_web_grok_image_annotation_enabled":false,"premium_content_api_read_enabled":false,"responsive_web_grok_community_note_auto_translation_is_enabled":false,"rweb_video_screen_enabled":false,"responsive_web_profile_redirect_enabled":false,"responsive_web_grok_show_grok_translated_post":false,"responsive_web_grok_imagine_annotation_enabled":false,"responsive_web_grok_analyze_button_fetch_trends_enabled":false,"responsive_web_grok_share_attachment_enabled":false,"profile_label_improvements_pcf_label_in_post_enabled":false,"responsive_web_jetfuel_frame":false,"responsive_web_grok_analysis_button_from_backend":false,"hidden_profile_subscriptions_enabled":false,"responsive_web_twitter_article_notes_tab_enabled":false,"subscriptions_feature_can_gift_premium":false,"subscriptions_verification_info_is_identity_verified_enabled":false,"payments_enabled":false}'
-
+  VARIABLES='{"screen_name":"'"${screen_name}"'","screenName":"'"${screen_name}"'","screen_names":["x","jack"],"rawQuery":"'"${query}"'","query":"'"${query}"'","userId":"'"${user_id}"'","userIds":["'"${user_id}"'","'"${user_id2}"'"],"rest_id":"'"${user_id}"'","postId":"'"${tweet_id}"'","focalTweetId":"'"${tweet_id}"'","tweetId":"'"${tweet_id}"'","tweet_id":"'"${tweet_id}"'","query_source":"typed_query","count":'"${count}"',"querySource":"typed_query","product":"'"${product}"'","listId":"'"${list_id}"'","includePromotedContent":false,"withBirdwatchNotes":true,"withVoice":true,"withCommunity":false,"listSlug":"'"${list_slug}"'","withDownvotePerspective":true,"withReactionsMetadata":true,"withReactionsPerspective":true,"includeHasBirdwatchNotes":true}'
+  FEATURES='{"android_graphql_skip_api_media_color_palette":false,"blue_business_profile_image_shape_enabled":false,"creator_subscriptions_subscription_count_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"freedom_of_speech_not_reach_fetch_enabled":false,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":false,"hidden_profile_likes_enabled":false,"highlights_tweets_tab_ui_enabled":false,"interactive_text_enabled":false,"longform_notetweets_consumption_enabled":true,"longform_notetweets_inline_media_enabled":false,"longform_notetweets_richtext_consumption_enabled":true,"longform_notetweets_rich_text_read_enabled":false,"responsive_web_edit_tweet_api_enabled":false,"responsive_web_enhance_cards_enabled":false,"responsive_web_graphql_exclude_directive_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"responsive_web_graphql_timeline_navigation_enabled":false,"responsive_web_media_download_video_enabled":false,"responsive_web_text_conversations_enabled":false,"responsive_web_twitter_article_tweet_consumption_enabled":false,"responsive_web_twitter_blue_verified_badge_is_enabled":true,"rweb_lists_timeline_redesign_enabled":true,"spaces_2022_h2_clipping":true,"spaces_2022_h2_spaces_communities":true,"standardized_nudges_misinfo":false,"subscriptions_verification_info_enabled":true,"subscriptions_verification_info_reason_enabled":true,"subscriptions_verification_info_verified_since_enabled":true,"super_follow_badge_privacy_enabled":false,"super_follow_exclusive_tweet_notifications_enabled":false,"super_follow_tweet_api_enabled":false,"super_follow_user_api_enabled":false,"tweet_awards_web_tipping_enabled":false,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":false,"tweetypie_unmention_optimization_enabled":false,"unified_cards_ad_metadata_container_dynamic_card_content_query_enabled":false,"verified_phone_label_enabled":false,"vibe_api_enabled":false,"view_counts_everywhere_api_enabled":false,"c9s_tweet_anatomy_moderator_badge_enabled":false,"rweb_video_timestamps_enabled":false,"rweb_tipjar_consumption_enabled":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"articles_preview_enabled":false,"tweet_with_visibility_results_prefer_gql_media_interstitial_enabled":false,"communities_web_enable_tweet_community_results_fetch":false,"responsive_web_grok_analyze_post_followups_enabled":false,"responsive_web_grok_image_annotation_enabled":false,"premium_content_api_read_enabled":false,"responsive_web_grok_community_note_auto_translation_is_enabled":false,"rweb_video_screen_enabled":false,"responsive_web_profile_redirect_enabled":false,"responsive_web_grok_show_grok_translated_post":false,"responsive_web_grok_imagine_annotation_enabled":false,"responsive_web_grok_analyze_button_fetch_trends_enabled":false,"responsive_web_grok_share_attachment_enabled":false,"profile_label_improvements_pcf_label_in_post_enabled":false,"responsive_web_jetfuel_frame":false,"responsive_web_grok_analysis_button_from_backend":false,"hidden_profile_subscriptions_enabled":false,"responsive_web_twitter_article_notes_tab_enabled":false,"subscriptions_feature_can_gift_premium":false,"subscriptions_verification_info_is_identity_verified_enabled":false,"payments_enabled":false,"immersive_video_status_linkable_timestamps":false,"profile_label_improvements_pcf_label_in_profile_enabled":false,"grok_translations_post_auto_translation_is_enabled":false,"grok_android_analyze_trend_fetch_enabled":false,"grok_translations_timeline_user_bio_auto_translation_is_enabled":false,"articles_api_enabled":false,"grok_translations_community_note_auto_translation_is_enabled":false,"grok_translations_community_note_translation_is_enabled":false}'
+  
   headers=(
     -H "Authorization: Bearer ${bearer_token}"
     -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-    -H "x-client-transaction-id: ${TID}"
     -H "X-Csrf-Token: ${x_csrf_token}"
     -H "Cookie: ct0=${x_csrf_token}; auth_token=${auth_token}"
   )
+  
+  # Use x-client-transaction-id header if using main web bearer token
+  if [[ "${bearer_token}" == *CpTnA ]]; then
+    TID=$(curl -s "https://x-client-transaction-id-generator.xyz/generate-x-client-transaction-id?path=${path}${endpoint}" | jq -r '."x-client-transaction-id"')
+    headers+=(-H "x-client-transaction-id: ${TID}")
+  fi
 
   output=$(curl -siG "${URL}" "${headers[@]}" \
     --data-urlencode "variables=${VARIABLES}" \
