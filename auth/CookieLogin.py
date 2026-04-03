a3e9edfafc00c3c9ef77a3c888faf250292354e9
cmj <129799+cmj@users.noreply.github.com>
2026-04-02 19:45:14 -0700

    Just pass token to script

--- auth/CookieLogin.py
+++ auth/CookieLogin.py
@@ -1,23 +1,27 @@
-#!/usr/bin/env python
-# Login to Twitter using only auth_token
+#!/usr/bin/env python3
+# Load a Twitter session using auth_token
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-import time
+import sys
 import os
 
-print("Twitter Login")
-auth_token = input("auth_token: ")
-print("Started. Console does not auto-close. Please close it manually.")
+try:
+    arg1 = sys.argv[1]
+except IndexError:
+    print("Usage: " + os.path.basename(__file__) + " <auth_token>")
+    sys.exit(1)
 
-options=Options()
-url = 'https://x.com/'
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+url = 'https://x.com/i/api/' # load dummy page for faster auth
+twurl = 'https://x.com'
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 driver.get(url)
+auth_token = sys.argv[1]
 driver.add_cookie({"name": "auth_token", "value": auth_token, "domain": ".x.com"})
-driver.get('https://x.com/settings/account')
-
-while True:
-    time.sleep(1)
+driver.get(twurl)
+#driver.refresh() # Refresh url to auth using cookies 
+#driver.quit()
