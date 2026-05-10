3e067efe7f214cc9f548f2269d31748bf9f1e5eb
cmj <129799+cmj@users.noreply.github.com>
2026-05-10 05:06:35 -0700

    tweet screenshot script

---
+++ misc/nitter-ss.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# take a screenshot of a tweet
+# requires: shot-scraper, imagemagick (to chop navbar)
+
+# nitter instance, see: https://github.com/zedeus/nitter/wiki/Instances
+#nitter="https://nitter.net"
+nitter="http://nitter.local" 
+
+url="$1"
+
+if [[ -z "$url" ]]; then
+  echo -e "Screenshot a tweet from Nitter\nUsage: $0 <url>"
+  exit 1
+fi
+
+id=${url#*/status/}
+id=${id%%[^0-9]*}
+
+#echo "Grabbing ${nitter}/i/status/${id}"
+
+# --retina doubles size
+shot-scraper -b firefox --retina -s '.main-thread' -h 2000 \
+  "${nitter}/i/status/${id}" -o - |
+  magick png:- -gravity north -chop 0x50 ${id}.png &&
+  echo "${id}.png saved"
