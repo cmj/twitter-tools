c505e853fc892df895ee37385561b67572ab0ac4
cmj <129799+cmj@users.noreply.github.com>
2026-05-10 06:56:35 -0700

    simplify screenshot script

--- misc/nitter-ss.sh
+++ misc/nitter-ss.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 # take a screenshot of a tweet
-# requires: shot-scraper, imagemagick (to chop navbar)
+# requires: shot-scraper
 
-# nitter instance, see: https://github.com/zedeus/nitter/wiki/Instances
+# set nitter instance, see: https://github.com/zedeus/nitter/wiki/Instances
 #nitter="https://nitter.net"
-nitter="http://nitter.local" 
+nitter="http://nitter.local"  
 
 url="$1"
 
 if [[ -z "$url" ]]; then
-  echo -e "Screenshot a tweet from Nitter\nUsage: $0 <url>"
+  echo -e "Screenshot a tweet from Nitter\nUsage: $0 <url or id>"
   exit 1
 fi
 
 id=${url#*/status/}
 id=${id%%[^0-9]*}
 
-#echo "Grabbing ${nitter}/i/status/${id}"
-
-# --retina doubles size. if used, set chop to 0x75, otherwise 0x50
-shot-scraper -b firefox --retina -s '.main-thread' -h 2000 \
-  "${nitter}/i/status/${id}" -o - |
-  magick png:- -gravity north -chop 0x75 ${id}.png &&
-  echo "${id}.png saved"
+# shot-scraper uses playwright: chromium (default), firefox, and webkit. set with -b option
+# --retina doubles size.
+shot-scraper \
+  -b firefox \
+  --retina \
+  --javascript '
+    if (document.querySelector(".before-tweet")) {
+      document.querySelector(".main-thread").id = "shot-target";
+    } else {
+      document.querySelector("nav").remove();
+      document.querySelector(".main-tweet .timeline-item").id = "shot-target";
+    }
+  ' \
+  -s '#shot-target' \
+  -h 2000 \
+  "${nitter}/i/status/${id}" -o "${id}.png"
