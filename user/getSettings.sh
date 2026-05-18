c1925dd4ad572b9d4f83598c2d9457621d0caa62
cmj <129799+cmj@users.noreply.github.com>
2026-05-18 02:57:43 -0700

    fix get settings, set content

--- user/getSettings.sh
+++ user/getSettings.sh
@@ -7,10 +7,10 @@ ct0=$(tr -dc 0-9a-f < /dev/urandom | head -c 32)
 if [[ -z $1 ]]; then echo "$0 <auth_token>"; exit 1; fi
 
 ###
-bearer_token="AAAAAAAAAAAAAAAAAAAAAIWCCAAAAAAA2C25AxqI%2BYCS7pdfJKRH8Xh19zA%3D8vpDZzPHaEJhd20MKVWp3UR38YoPpuTX7UD2cVYo3YNikubuxd"
+bearer_token="AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F"
 
 curl -s 'https://api.x.com/1.1/account/settings.json?include_ext_sharing_audiospaces_listening_data_with_followers=true&include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&include_ext_dm_av_call_settings=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true' \
-  -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
+  -H "User-Agent: TwitterAndroid/10.21.1" \
   -H "x-csrf-token: ${ct0}" \
   -H "authorization: Bearer ${bearer_token}" \
   -H "Cookie: ct0=${ct0}; auth_token=${auth_token}" |
--- user/nsfw.sh
+++ user/nsfw.sh
@@ -8,10 +8,10 @@ if [[ -z $1 ]]; then echo -e "Enable sensitive content\n$0 <auth_token>"; exit 1
 
 ###
 ct0=$(tr -dc 0-9a-f < /dev/urandom | head -c 32)
-bearer_token="AAAAAAAAAAAAAAAAAAAAAIWCCAAAAAAA2C25AxqI%2BYCS7pdfJKRH8Xh19zA%3D8vpDZzPHaEJhd20MKVWp3UR38YoPpuTX7UD2cVYo3YNikubuxd"
+bearer_token="AAAAAAAAAAAAAAAAAAAAAFXzAwAAAAAAMHCxpeSDG1gLNLghVe8d74hl6k4%3DRUMF4xAQLsbeBhTSRrCiQpJtxoGWeyHrDb5te2jpGskWDFW82F"
 
 curl -s https://api.x.com/1.1/account/settings.json \
-  -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
+  -H "User-Agent: TwitterAndroid/10.21.1" \
   -H "x-csrf-token: ${ct0}" \
   -H "authorization: Bearer ${bearer_token}" \
   -H "Cookie: ct0=${ct0}; auth_token=${auth_token}" \
